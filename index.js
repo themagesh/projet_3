481f2d2d32e12d1d440d273865626509039286ec
themagesh <themagesh@gmail.com>
2024-06-21 06:24:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const sidenav = document.querySelector('.side-navbar')
+const sidenv = document.querySelector('.navbar-manu-toggle')
+function showNavbar()
+{
+  sidenav.style.left = '0'
+  
+}
+function closeNavbar()
+{
+    sidenav.style.left ='-60%'
+}
